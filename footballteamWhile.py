23cd8bfd2d90243370d1abcd05502fc234cbc3e4
kanyanatfayfay <116140957+kanyanatfayfay@users.noreply.github.com>
2022-12-06 12:08:54 +0700

    Update footballteamWhile.py

--- footballteamWhile.py
+++ footballteamWhile.py
@@ -1,14 +1,10 @@
 football_team = []
 
-#TO-DO:append 11 players name
 for i in range(11):
     name = input("Add a player to the football team: ")
     football_team.append(name)
     print(football_team)
 
-#TO-DO:edit an item.    [Me;(9-11)"this is QUESTION"]
-#TO-DO:delete an item.
-#Ask user which item to change/delete.
 repeat = 'Y'
 while repeat == 'Y':  
     edit = int(input("Which players do you want to change?: ")) 
